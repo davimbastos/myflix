5cb9ee37139df357fe357a18c703cc5a40b3bed0
davimbastos <davimbastos@gmail.com>
2020-07-31 19:20:48 -0300

    Criação dos métodos para cadastro dos vídeos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,48 +1,55 @@
-import React from 'react';
-import Menu from '../../components/Menu'
-import dadosIniciais from '../../data/dados_iniciais.json';
+import React, { useState, useEffect } from 'react';
+import PageDefault from '../../components/PageDefault';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import categoriesRepository from '../../repositories/categories';
 
 function Home() {
+  const [initialData, setInitialData] = useState([]);
+
+  useEffect(() => {
+    categoriesRepository.getAllWithVideos()
+      .then((categoriesWithVideos) => {
+        setInitialData(categoriesWithVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div style={{ background: "#141414" }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
-      />
+      {initialData.length === 0 && (<div>Loading...</div>)}
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+      {/* {JSON.stringify(initialData)} */}
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+      {initialData.map((category, index) => {
+        if (index === 0) {
+          return (
+            <div key={category.id}>
+              <BannerMain
+                videoTitle={initialData[0].videos[0].title}
+                url={initialData[0].videos[0].url}
+                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={initialData[0]}
+              />
+            </div>
+          );
+        }
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+        return (
+          <Carousel
+            key={category.id}
+            category={category}
+          />
+        );
+      })}
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
-
-      <Footer />
-    </div>
+    </PageDefault>
   );
 }
 
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-named-as-default
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
+import categoriesRepository from '../../../repositories/categories';
 
 function CadastroVideo() {
-    return (
-      <PageDefault>
-        <h1>Cadastro de Vídeo</h1>
+  const history = useHistory();
+  const [categories, setCategories] = useState([]);
+  const categoryTitles = categories.map(({ title }) => title);
+  const { handleChange, values } = useForm({
+    title: '',
+    url: '',
+    category: '',
+  });
 
-        <Link to="/cadastro/categoria">
-          Cadastrar Categoria
-        </Link>
-      </PageDefault>
-    )
-  }
+  useEffect(() => {
+    categoriesRepository
+      .getAll()
+      .then(() => {
+        setCategories(categories);
+      });
+  }, []);
 
-  export default CadastroVideo;
+  console.log(categoryTitles);
+  return (
+    <PageDefault>
+      <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        // eslint-disable-next-line no-alert
+        // alert('Video cadastrado com sucesso!');
+
+        const categorySelected = categories.find((category) => category.title === values.category);
+
+        videosRepository.create({
+          title: values.title,
+          url: values.url,
+          categoryId: categorySelected,
+        })
+          .then(() => {
+            history.push('/');
+          });
+      }}
+      >
+        <FormField
+          label="Título do Vídeo: "
+          name="title"
+          value={values.title}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL do Vídeo: "
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Categoria: "
+          name="category"
+          value={values.category}
+          onChange={handleChange}
+          suggestions={categoryTitles}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroVideo;

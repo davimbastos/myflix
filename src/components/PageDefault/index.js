eb523860dafb31332b86d309372beac0d84fa558
davimbastos <davimbastos@gmail.com>
2020-08-01 11:16:35 -0300

    Separação dos styles

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 import Menu from '../Menu';
 import Footer from '../Footer';
-
-const Main = styled.main`
-    background-color: var(--black);
-    color: var(--white);
-    flex: 1;
-    padding-top: 50px;
-    padding-left: 5%;
-    padding-right: 5%;
-    ${({ paddingAll }) => css`
-      padding: ${paddingAll};
-    `}
-`;
+import { Main } from './styles';
 
 function PageDefault({ children, paddingAll }) {
   return (
---
+++ src/components/PageDefault/styles.js
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Main = styled.main`
+    background-color: var(--black);
+    color: var(--white);
+    flex: 1;
+    padding-top: 50px;
+    padding-left: 5%;
+    padding-right: 5%;
+    ${({ paddingAll }) => css`
+    padding: ${paddingAll};
+    `}
+`;
